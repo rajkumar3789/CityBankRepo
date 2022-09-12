836b14e5ea5ee25ffec9a0acaa67472b23dae800
dev1 <dev@gmail.com>
2022-09-12 15:53:18 +0530

    Test.class changed

--- DemoProject/src/com/infosys/test/Test.java
+++ DemoProject/src/com/infosys/test/Test.java
@@ -1,6 +1,10 @@
 package com.infosys.test;
 
+import com.infosys.beans.Utility;
+
 public class Test {
-
-
-}
+	public static void main(String[] args) {
+	Utility util=new Utility();
+	System.out.println(util.getByDate());
+	}//main
+}//class
